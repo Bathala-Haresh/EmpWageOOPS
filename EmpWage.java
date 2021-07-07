422da4be215863ce3e9e463d8fd5af4046ed7757
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-08 00:51:49 +0530

    welcome message

---
+++ EmpWage.java
@@ -0,0 +1,9 @@
+package com.java.problems;
+
+public class EmpWage 
+{
+     public static void main(String args[])
+        {
+              System.out.println("Welcome to employee emp wage problem");
+        }
+}
