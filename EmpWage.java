854e0c72f59645e0e751617b9a10b571a2e67e0a
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-08 00:55:53 +0530

    checking employee present or not

--- EmpWage.java
+++ EmpWage.java
@@ -1,9 +1,36 @@
 package com.java.problems;
 
-public class EmpWage 
+public class EmployeeWage1 {
+
+	void method()
+	{
+		System.out.println("Attendence Checking ....");
+
+		//Random function to get either 1 or 0
+		int employee=(int)(Math.random() * 100) % 2;
+
+		if(employee == 1)
+		{
+			System.out.print("present");
+		}
+		else{
+			System.out.print("absent");
+		}
+	}
+
+}
+
+
+
+public class EmpWage
 {
-     public static void main(String args[])
-        {
-              System.out.println("Welcome to employee emp wage problem");
-        }
+	public static void main(String args[])
+	{
+
+		EmployeeWage1 emp1=new EmployeeWage1();
+		emp1.method();
+		
+		
+		
+	}
 }
