4f504f573d127736517876fc6a2b7022b8bdbf00
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-08 01:07:17 +0530

    part time including

--- EmpWage.java
+++ EmpWage.java
@@ -1,35 +1,43 @@
 package com.java.problems;
 
-public class EmployeeWageUc2 {
-
+public class EmployeeWageUc3 {
 	final int fullTime = 1;
+	final int partTime = 2;
+	final int wageHr = 20;
 	public int getFullTime() {
 		return fullTime;
+	}
+	public int getPartTime() {
+		return partTime;
 	}
 	public int getWageHr() {
 		return wageHr;
 	}
-	final int wageHr = 20;
+
 
 	void method()
 	{
-		int empType = (int) (Math.random() * 100) % 2;
+		int empType = (int) (Math.random() * 100) % 3;
 		int workingHours = 0;
 
-		//If condition to check employee is present or absent
+
 		if (empType == fullTime)
 		{
-			System.out.println("Employee is Present so ...");
+			System.out.println("Employee is Present so ");
 			workingHours = 8;
-		} else
+		} 
+		else if (empType == partTime) 
 		{
-			System.out.println("Employee is Absent so ...");
+			System.out.println("Employee has present PartTime so ");
+			workingHours = 4;
+		}else
+		{
+			System.out.println("Employee is Absent so ");
 		}
-		//calculating the wage
+		//calculate the wage of an employee
 		int wage = workingHours * wageHr;
 		System.out.println("Employee Daily Wage is " + wage);
 	}
-
 }
 
 
@@ -41,9 +49,8 @@ public class EmpWage
 		
 		
 		System.out.println("--------------------------------------------------");
-		EmployeeWageUc2 emp2=new EmployeeWageUc2();
-		emp2.method();
-		
+		EmployeeWageUc3 emp3=new EmployeeWageUc3();
+		emp3.method();
 		
 		
 		
